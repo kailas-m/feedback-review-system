5ee4ca7a7a8cbf2c9134a45314893f74cce296da
kailas-m <149128168+kailas-m@users.noreply.github.com>
2025-12-30 15:13:32 +0530

    Update admin.py

--- feedback/reviews/admin.py
+++ feedback/reviews/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Review, Permission, Role, UserPermission
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "rating", "created_at")
+    search_fields = ("user__username", "comment")
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ("codename", "name", "allowed_for_moderator")
+    search_fields = ("codename", "name")
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    filter_horizontal = ("permissions", "users")
+
+@admin.register(UserPermission)
+class UserPermissionAdmin(admin.ModelAdmin):
+    list_display = ("user", "permission")
+    search_fields = ("user__username", "permission__codename")
