59c51f45fb4eac44923bf68d4e5d12e6f26d90a9
kailas-m <149128168+kailas-m@users.noreply.github.com>
2025-12-30 15:14:16 +0530

    Add role management and average rating URLs

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -8,10 +8,15 @@ urlpatterns = [
     path("submit/", views.SubmitReviewView.as_view()),
     path("update/<int:id>/", views.UpdateReviewView.as_view()),
     path("delete/<int:id>/", views.DeleteReviewView.as_view()),
-
     path("list/", views.ListReviewsView.as_view()),
     path("filter/", views.FilterReviewsView.as_view()),
+    path("average-rating/", views.AverageRatingView.as_view()),
 
     path("admin-reply/<int:id>/", views.AdminReplyView.as_view()),
-    path("average-rating/", views.AverageRatingView.as_view()),
+    path("roles/", views.ListRolesView.as_view()),
+    path("roles/promote/", views.PromoteToModeratorView.as_view()),
+    path("roles/modify-permission-user/", views.ModifyPermissionUserView.as_view()),
+    path("roles/modify-permission-role/", views.ModifyPermissionRoleView.as_view()),
+    path("roles/demote/", views.DemoteModeratorView.as_view()),
+    path("moderator/modify-permission-user/", views.ModeratorModifyPermissionUserView.as_view()),
 ]
